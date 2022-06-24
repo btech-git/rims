e055a2f454200e6b8be149d7ec7be9ad3d7f0c0e
pmahendro <pmahendro@bloomingtech-server>
2022-06-24 19:32:19 +0700

    Fixed asset additional

--- protected/modules/frontDesk/views/materialRequest/admin.php
+++ protected/modules/frontDesk/views/materialRequest/admin.php
@@ -8,7 +8,7 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-    array('label'=>'List MaterialRequest', 'url'=>array('index')),
+    //array('label'=>'List MaterialRequest', 'url'=>array('index')),
     array('label'=>'Create MaterialRequest', 'url'=>array('create')),
 );
 
