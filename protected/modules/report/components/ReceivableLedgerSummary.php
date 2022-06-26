8275125136f9f97316b801f8b324b279ccc1bf81
pmahendro <pmahendro@bloomingtech-server>
2022-06-26 21:58:56 +0700

    Fixed asset additional

--- protected/modules/report/components/ReceivableLedgerSummary.php
+++ protected/modules/report/components/ReceivableLedgerSummary.php
@@ -17,7 +17,7 @@ class ReceivableLedgerSummary extends CComponent {
     }
 
     public function setupPaging($pageSize, $currentPage) {
-        $pageSize = (empty($pageSize)) ? 10000 : $pageSize;
+        $pageSize = (empty($pageSize)) ? 7000 : $pageSize;
 //        $pageSize = ($pageSize <= 0) ? 1 : $pageSize;
         $this->dataProvider->pagination->pageSize = $pageSize;
 
