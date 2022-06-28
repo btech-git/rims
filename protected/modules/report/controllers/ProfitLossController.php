12fb2ab28c5de9667a1491c816db3f8ca29026cb
pmahendro <pmahendro@bloomingtech-server>
2022-06-28 14:20:54 +0700

    Fixed asset additional

--- protected/modules/report/controllers/ProfitLossController.php
+++ protected/modules/report/controllers/ProfitLossController.php
@@ -28,8 +28,9 @@ class ProfitLossController extends Controller {
 
         $accountCategoryTypes = CoaCategory::model()->findAll(array('condition' => 't.id BETWEEN 6 AND 10'));
 
-        if (isset($_GET['SaveExcel']))
+        if (isset($_GET['SaveExcel'])) {
             $this->saveToExcel($accountCategoryTypes, $startDate, $endDate, $branchId);
+        }
 
         $this->render('summary', array(
             'accountCategoryTypes' => $accountCategoryTypes,
@@ -39,4 +40,105 @@ class ProfitLossController extends Controller {
         ));
     }
 
+    protected function saveToExcel($accountCategoryTypes, $startDate, $endDate, $branchId) {
+        $startDate = (empty($startDate)) ? date('Y-m-d') : $startDate;
+        $endDate = (empty($endDate)) ? date('Y-m-d') : $endDate;
+        $startDateString = Yii::app()->dateFormatter->format('d MMMM yyyy', $startDate);
+        $endDateString = Yii::app()->dateFormatter->format('d MMMM yyyy', $endDate);
+
+        spl_autoload_unregister(array('YiiBase', 'autoload'));
+        include_once Yii::getPathOfAlias('ext.phpexcel.Classes') . DIRECTORY_SEPARATOR . 'PHPExcel.php';
+        spl_autoload_register(array('YiiBase', 'autoload'));
+
+        $objPHPExcel = new PHPExcel();
+
+        $documentProperties = $objPHPExcel->getProperties();
+        $documentProperties->setCreator('Raperind Motor');
+        $documentProperties->setTitle('Laporan Profit Loss Induk');
+
+        $worksheet = $objPHPExcel->setActiveSheetIndex(0);
+        $worksheet->setTitle('Laporan Profit Loss Induk');
+
+        $worksheet->mergeCells('A1:B1');
+        $worksheet->mergeCells('A2:B2');
+        $worksheet->mergeCells('A3:B3');
+        $worksheet->getStyle('A1:B3')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+        $worksheet->getStyle('A1:B3')->getFont()->setBold(true);
+
+        $branch = Branch::model()->findByPk($branchId);
+        $worksheet->setCellValue('A1', 'Laporan Profit Loss Induk');
+        $worksheet->setCellValue('A2', CHtml::encode(($branch === null) ? '' : $branch->name));
+        $worksheet->setCellValue('A3', $startDateString . ' - ' . $endDateString);
+
+        $counter = 5;
+
+        $profitLossAmount = 0.00;
+        foreach ($accountCategoryTypes as $accountCategoryType) {
+            $accountCategoryTypeBalance = 0.00;
+            $worksheet->getStyle("A{$counter}")->getFont()->setBold(true);
+            $worksheet->setCellValue("A{$counter}", CHtml::encode(CHtml::value($accountCategoryType, 'code')));
+            $worksheet->setCellValue("B{$counter}", CHtml::encode(CHtml::value($accountCategoryType, 'name')));
+
+            $counter++;
+
+            $coaSubCategories = CoaSubCategory::model()->findAllByAttributes(array('coa_category_id' => $accountCategoryType->id), array('order' => 'code ASC'));
+            foreach ($coaSubCategories as $accountCategory) {
+                $accountCategoryBalance = 0.00;
+                $coas = Coa::model()->findAllByAttributes(array('coa_sub_category_id' => $accountCategory->id, 'is_approved' => 1, 'coa_id' => null), array('order' => 'code ASC'));
+                foreach ($coas as $coa) {
+                    $accountGroupBalance = 0.00;
+                    foreach ($coa->coaIds as $account) {
+                        $accountBalance = $account->getProfitLossBalance($startDate, $endDate, $branchId);
+                        $accountGroupBalance += $accountBalance;
+                    }
+                    $accountCategoryBalance += $accountGroupBalance;
+                }
+                $worksheet->setCellValue("A{$counter}", CHtml::encode(CHtml::value($accountCategory, 'code')));
+                $worksheet->setCellValue("B{$counter}", CHtml::encode(CHtml::value($accountCategory, 'name')));
+                $worksheet->getStyle("C{$counter}")->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_RIGHT);
+                $worksheet->setCellValue("C{$counter}", CHtml::encode($accountCategoryBalance));
+                $counter++;
+                
+                if ((int)$accountCategory->id === 28 || (int)$accountCategory->id === 30 || (int)$accountCategory->id === 31){
+                     $accountCategoryTypeBalance -= $accountCategoryBalance;
+                } else {
+                     $accountCategoryTypeBalance += $accountCategoryBalance;
+                }
+            }
+
+            $worksheet->getStyle("A{$counter}:B{$counter}")->getBorders()->getTop()->setBorderStyle(PHPExcel_Style_Border::BORDER_THICK);
+            $worksheet->getStyle("A{$counter}:B{$counter}")->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_RIGHT);
+            $worksheet->setCellValue("A{$counter}", 'TOTAL ' . CHtml::encode(CHtml::value($accountCategoryType, 'name')));
+            $worksheet->setCellValue("C{$counter}", CHtml::encode($accountCategoryTypeBalance));
+
+            $counter++;
+            $counter++;
+            
+            if ($accountCategoryType->id == 7 || $accountCategoryType->id == 8 || $accountCategoryType->id == 10) {
+                $profitLossAmount -= $accountCategoryTypeBalance;
+            } else {
+                $profitLossAmount += $accountCategoryTypeBalance;
+            }
+        }
+
+        $worksheet->getStyle("A{$counter}:B{$counter}")->getBorders()->getTop()->setBorderStyle(PHPExcel_Style_Border::BORDER_THICK);
+        $worksheet->getStyle("A{$counter}:B{$counter}")->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_RIGHT);
+        $worksheet->setCellValue("A{$counter}", 'PROFIT / LOSS ');
+        $worksheet->setCellValue("C{$counter}", CHtml::encode($profitLossAmount));
+
+        for ($col = 'A'; $col !== 'C'; $col++) {
+            $objPHPExcel->getActiveSheet()
+            ->getColumnDimension($col)
+            ->setAutoSize(true);
+        }
+
+        header('Content-Type: application/xlsx');
+        header('Content-Disposition: attachment;filename="Laporan Profit Loss Induk.xlsx"');
+        header('Cache-Control: max-age=0');
+
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+        $objWriter->save('php://output');
+
+        Yii::app()->end();
+    }
 }
