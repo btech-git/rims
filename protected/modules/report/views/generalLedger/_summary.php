689e932916ac3ae6e2adc94f5efa5812558ab3d1
pmahendro <pmahendro@bloomingtech-server>
2022-07-20 22:38:48 +0700

    save to excel fixed

--- protected/modules/report/views/generalLedger/_summary.php
+++ protected/modules/report/views/generalLedger/_summary.php
@@ -104,10 +104,10 @@ Yii::app()->clientScript->registerCss('_report', '
                     <?php endforeach; ?>
                         <tr>
                             <td colspan="4" style="text-align: right">Total</td>
-                            <td class="width2-6" style="text-align: right">
+                            <td class="width2-6" style="text-align: right; border-top: 2px solid">
                                 <?php echo CHtml::encode(Yii::app()->numberFormatter->format('#,##0', $totalDebit)); ?>
                             </td>
-                            <td class="width2-7" style="text-align: right">
+                            <td class="width2-7" style="text-align: right; border-top: 2px solid">
                                 <?php echo CHtml::encode(Yii::app()->numberFormatter->format('#,##0', $totalCredit)); ?>
                             </td>
                             <td>&nbsp;</td>
