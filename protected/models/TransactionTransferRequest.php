c7637495cd50424839d54a4d6c9b05539e236bb1
pmahendro <pmahendro@bloomingtech-server>
2022-07-26 14:00:22 +0700

    save to excel fixed

--- protected/models/TransactionTransferRequest.php
+++ protected/models/TransactionTransferRequest.php
@@ -220,7 +220,7 @@ class TransactionTransferRequest extends MonthlyTransactionActiveRecord {
         $criteria->condition = "EXISTS (
             SELECT COALESCE(SUM(d.quantity_delivery_left), 0) AS quantity_remaining
             FROM " . TransactionTransferRequestDetail::model()->tableName() . " d
-            WHERE t.id = d.transfer_request_id AND status_document = 'Approved'
+            WHERE t.id = d.transfer_request_id AND status_document NOT IN ('Draft', 'Rejected')
             GROUP BY d.transfer_request_id
             HAVING quantity_remaining > 0
         )";
