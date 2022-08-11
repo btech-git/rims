5cd00d730cd65d0e5d8b3ad73b33b75f0305c083
pmahendro <pmahendro@bloomingtech-server>
2022-08-11 20:02:18 +0700

    save to excel fixed

--- protected/modules/accounting/views/paymentOut/_detail.php
+++ protected/modules/accounting/views/paymentOut/_detail.php
@@ -40,7 +40,7 @@
                 
                 <td style="text-align: right">
                     <?php echo CHtml::activeHiddenField($detail, "[$i]total_invoice"); ?>
-                    <?php echo CHtml::encode(Yii::app()->numberFormatter->format('#,##0', CHtml::value($receiveItem, 'grandTotal'))); ?>
+                    <?php echo CHtml::encode(Yii::app()->numberFormatter->format('#,##0.00', CHtml::value($receiveItem, 'grandTotal'))); ?>
                     <?php echo CHtml::error($detail, 'invoice_grand_total'); ?>
                 </td>
 
