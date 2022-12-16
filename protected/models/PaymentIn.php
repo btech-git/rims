36f730f29ac90ebc66184b7bf8a876423bb19bab
pmahendro <pmahendro@bloomingtech-tp>
2022-12-16 22:16:32 +0700

    Add entity transaction purchase request

--- protected/models/PaymentIn.php
+++ protected/models/PaymentIn.php
@@ -335,7 +335,7 @@ class PaymentIn extends MonthlyTransactionActiveRecord {
                 'defaultOrder' => 'payment_date DESC',
             ),
             'pagination' => array(
-                'pageSize' => 200,
+                'pageSize' => 150,
             ),
         ));
     }
