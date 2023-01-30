e3f80c39355e29d6232d38718fa651196bf0c7b5
pmahendro <pmahendro@bloomingtech-tp>
2023-01-30 15:31:41 +0700

    Modify textbox size on grid and view

--- protected/modules/report/controllers/SaleOrderController.php
+++ protected/modules/report/controllers/SaleOrderController.php
@@ -148,11 +148,13 @@ class SaleOrderController extends Controller {
             }
         }
 
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+        ob_end_clean();
+
         header('Content-type: application/vnd.ms-excel');
         header('Content-Disposition: attachment;filename="Laporan Sale Order.xlsx"');
         header('Cache-Control: max-age=0');
 
-        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
         $objWriter->save('php://output');
 
         Yii::app()->end();
