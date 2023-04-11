c7e7421826402227fe236fce9ca4ae61fbbdb43e
pmahendro <pmahendro@bloomingtech-tp>
2023-04-11 21:53:04 +0700

    Modify memo transaction

--- protected/modules/report/controllers/ProfitLossMonthlyController.php
+++ protected/modules/report/controllers/ProfitLossMonthlyController.php
@@ -190,7 +190,7 @@ class ProfitLossMonthlyController extends Controller {
         ob_end_clean();
         // We'll be outputting an excel file
         header('Content-type: application/vnd.ms-excel');
-        header('Content-Disposition: attachment;filename="Profit Loss per Periode.xls"');
+        header('Content-Disposition: attachment;filename="Profit Loss per Periode.xlsx"');
         header('Cache-Control: max-age=0');
         
         $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
